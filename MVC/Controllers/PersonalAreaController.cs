92170dc8ce0c599cd91be17d11981668c7556efd
adi Stamker <adistamker88@gmail.com>
2024-12-31 02:36:28 +0200

    pesonal area

---
+++ MVC/Controllers/PersonalAreaController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MyMvcProject.Data;
+
+namespace MyMvcProject.Controllers
+{
+    public class PersonalAreaController : Controller
+    {
+        private readonly MvcProjectContext db;
+
+        public PersonalAreaController()
+        {
+            db = new MvcProjectContext();
+        }
+
+        public ActionResult Index()
+        {
+            // הפעולה לדף האזור האישי
+            return View();
+        }
+    }
+}
