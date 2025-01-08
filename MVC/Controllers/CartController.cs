74ea31e525ef0055d1cf8084f84b8cde94c148a5
adi stamker <adistamker88@gmail.com>
2025-01-08 02:54:36 +0200

    תיקונים לעגלת קניות כלל חיבור בין עמוד תשלום לעגלה

--- MVC/Controllers/CartController.cs
+++ MVC/Controllers/CartController.cs
@@ -1,35 +1,61 @@
-﻿using MVC.Models;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVC.Controllers
 {
     public class CartController : Controller
     {
+        // הצגת עמוד העגלה
         public ActionResult Index()
         {
-            var cart = Session["Cart"] as List<CartItem> ?? new List<CartItem>();
+            var cart = Session["Cart"] as List<Dictionary<string, object>> ?? new List<Dictionary<string, object>>();
             return View(cart);
         }
 
+        // הוספת פריט לעגלה
+        [HttpPost]
+        public JsonResult AddToCart(int bookId, string bookName, decimal price, string type)
+        {
+            var cart = Session["Cart"] as List<Dictionary<string, object>> ?? new List<Dictionary<string, object>>();
+
+            var existingItem = cart.FirstOrDefault(item => (int)item["BookId"] == bookId && (string)item["Type"] == type);
+            if (existingItem != null)
+            {
+                existingItem["Quantity"] = (int)existingItem["Quantity"] + 1;
+            }
+            else
+            {
+                cart.Add(new Dictionary<string, object>
+                {
+                    { "BookId", bookId },
+                    { "BookName", bookName },
+                    { "Price", price },
+                    { "Quantity", 1 },
+                    { "Type", type }
+                });
+            }
+
+            Session["Cart"] = cart;
+
+            return Json(new { success = true, cartCount = cart.Count });
+        }
+
+        // הסרת פריט מהעגלה
         [HttpPost]
         public JsonResult RemoveFromCart(int bookId, string type)
         {
-            var cart = Session["Cart"] as List<CartItem>;
-            if (cart != null)
+            var cart = Session["Cart"] as List<Dictionary<string, object>> ?? new List<Dictionary<string, object>>();
+
+            var itemToRemove = cart.FirstOrDefault(item => (int)item["BookId"] == bookId && (string)item["Type"] == type);
+            if (itemToRemove != null)
             {
-                var item = cart.FirstOrDefault(c => c.BookId == bookId && c.Type == type);
-                if (item != null)
-                {
-                    cart.Remove(item);
-                    Session["Cart"] = cart;
-                }
+                cart.Remove(itemToRemove);
             }
+
+            Session["Cart"] = cart;
+
             return Json(new { success = true });
         }
     }
-
 }
