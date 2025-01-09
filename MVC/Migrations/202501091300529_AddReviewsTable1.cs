b6e5b6656314204c9d6049465cbb6b5b59e713cf
adi stamker <adistamker88@gmail.com>
2025-01-09 15:05:40 +0200

    ביצוע של מגרציה ותיקון של עגלות וכל זה

---
+++ MVC/Migrations/202501091300529_AddReviewsTable1.cs
@@ -0,0 +1,18 @@
+﻿namespace MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddReviewsTable1 : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
